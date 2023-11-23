e72e5c4cc81ebc64666aa7399dd6e22abe89b8a9
Amadou-Marong <marongamadou6@gmail.com>
2023-11-23 12:25:43 +0000

    push

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,15 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/testing', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
 
 
 // Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
